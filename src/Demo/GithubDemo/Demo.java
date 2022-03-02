122658e6b7d018a4f21fb4fe8492a1c7484a09ff
DoguAkk <100165383+DoguAkk@users.noreply.github.com>
2022-03-02 02:55:46 +0100

    Initial commit

--- src/Demo/GithubDemo/Demo.java
+++ src/Demo/GithubDemo/Demo.java
@@ -1,7 +1,8 @@
-package WerkboekOpdrachten.Demo.GithubDemo;
+package Demo.GithubDemo;
 
 public class Demo {
     public static void main(String[] args){
         System.out.println("Test");
+        System.out.println("Test_2");
     }
 }
