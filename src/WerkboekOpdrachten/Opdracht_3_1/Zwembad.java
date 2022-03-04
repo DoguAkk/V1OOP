8de6eea509acbc412906f8482a21bd0b4fcf5546
DoguAkk <100165383+DoguAkk@users.noreply.github.com>
2022-03-04 21:55:28 +0100

    Initial commit

---
+++ src/WerkboekOpdrachten/Opdracht_3_1/Zwembad.java
@@ -0,0 +1,50 @@
+package WerkboekOpdrachten.Opdracht_3_1;
+
+public class Zwembad {
+    private double breedte;
+    private double lengte;
+    private double diepte;
+
+    public Zwembad(double br, double le, double di){
+        breedte = br;
+        lengte = le;
+        diepte = di;
+    }
+
+    public Zwembad(){
+
+    }
+
+    public double getBreedte() {
+        return breedte;
+    }
+
+    public double getLengte() {
+        return lengte;
+    }
+
+    public double getDiepte() {
+        return diepte;
+    }
+
+    public void setBreedte(double br) {
+        breedte = br;
+    }
+
+    public void setLengte(double le) {
+        lengte = le;
+    }
+
+    public void setDiepte(double di) {
+        diepte = di;
+    }
+
+    public double inhoud(){
+        return diepte * lengte * breedte;
+    }
+
+    @Override
+    public String toString() {
+        return "Dit zwembad is " + getBreedte() + " meter breed, " + getLengte() + " meter lang, en " + getDiepte() + " meter diep";
+    }
+}
